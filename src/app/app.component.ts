31cc2881b6f4669d9b6af0d2fbeebc0aaa3b8d7a
MGYOSBEL <mgyosbel@gmail.com>
2019-11-25 17:41:30 -0500

    first commit to JobbagDemo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment.prod';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Jobbag';
+  constructor() { }
+
+  ngOnInit() {
+    if ('serviceWorker' in navigator  && environment.production) {
+      window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+          // Registration was successful
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+          // registration failed :(
+          console.log('ServiceWorker registration failed: ', err);
+        });
+      });
+    }
+    
+  }
+  
 }
